fedbd755fa1c898d4ef24f37c29e141b6a2b8826
preethisiva1983 <142735408+preethisiva1983@users.noreply.github.com>
2023-08-20 21:09:55 +0530

    Add files via upload
    Day1 assignments

---
+++ day01/HelloSelenium.java
@@ -0,0 +1,8 @@
+package week01.day01;
+
+public class HelloSelenium {
+	public static void main(String[] args) {
+		System.out.println("Hi, First java code");
+	}
+
+}
---
+++ day01/Informations.java
@@ -0,0 +1,25 @@
+package week01.day01;
+
+public class Informations {
+	
+	public static void main(String[] args) {
+		int value = 100;
+		long PhoneNum = 930200212l;
+		float height = 35.56f;
+		double weight = 345.4568790;
+		boolean isVerified = true;
+		char logo = 'M';
+		String Detail = "Details";
+		
+		System.out.println("The value is " +value);
+		System.out.println("The Phone number is " +PhoneNum);
+		System.out.println("The height is " +height);
+
+		System.out.println("The flag is " +isVerified);
+		System.out.println("The logo is " +logo);
+		System.out.println("The Detail is " +Detail);
+		System.out.println("The Weight is " +weight);
+		
+	}
+
+}
